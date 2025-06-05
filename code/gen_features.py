38f085dc5f41e369f8e495e4f1a0bcedecba8e12
Bozhen Hu <hubozhen@westlake.edu.cn>
2025-06-05 23:06:39 +0800

    Update gen_features.py

--- code/gen_features.py
+++ code/gen_features.py
@@ -6,7 +6,7 @@ from feature_functions import *
 import argparse
 import os
 '''
-Generate features using get_features() in /code/feature_functions.py.
+Generate features using get_features() in code/feature_functions.py.
 '''
 
 if __name__ == "__main__":
@@ -18,12 +18,12 @@ if __name__ == "__main__":
                                      --dict_path: path to dictionary dir; \
                                      --has_label: whether the data has labels')
     
-    parser.add_argument('--data', type=str, default='/usr/data/DLTKcat/data/test_data.csv')
-    parser.add_argument('--output', type=str , default='/usr/data/DLTKcat/data/test_features_esm')
+    parser.add_argument('--data', type=str, default='../data/train_data.csv')
+    parser.add_argument('--output', type=str , default='../data/train_features_esm_seq')
     parser.add_argument('--radius', default = 2, type=int )
     parser.add_argument('--ngram', default = 3, type=int )
     parser.add_argument('--has_dict', type=str, choices=['False','True'], default = 'True')
-    parser.add_argument('--dict_path', type=str, default = '/usr/data/DLTKcat/data/dict')
+    parser.add_argument('--dict_path', type=str, default = '../data/dict')
     parser.add_argument('--has_label', type=str, choices=['False','True'], default = 'True')
     args = parser.parse_args()
     
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     print([data_path, output_path, radius, ngram, has_dict ,dict_path, has_label])
     # get_features( data_path, output_path, radius, ngram, has_dict, dict_path, has_label )
     get_esm_features( data_path, output_path, radius, ngram, has_dict, dict_path, has_label )
+    # get_esm_seq_features( data_path, output_path, radius, ngram, has_dict, dict_path, has_label )
     print('Feature generation completed.')
     
     
